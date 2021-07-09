d7fa71bd00bf0e0125707f5d3a6d5d618d0c79b3
Ademir <ademir.m.junior@gmail.com>
2021-07-09 10:32:15 -0300

    Developed Macther tests proof

---
+++ src/main/java/one/digitalinnovation/pessoa/Pessoa.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.pessoa;
+
+public class Pessoa {
+    
+    private String nome;
+    private String sobrenome;
+
+    public Pessoa(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+}
--- src/test/java/one/digitalinnovation/test/MatcherTest.java
+++ src/test/java/one/digitalinnovation/test/MatcherTest.java
@@ -1,5 +1,47 @@
 package one.digitalinnovation.test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.text.IsEmptyString.emptyOrNullString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.hamcrest.beans.HasProperty;
+import org.hamcrest.object.HasToString;
+import org.junit.Test;
+
+import one.digitalinnovation.pessoa.Pessoa;
+
 public class MatcherTest {
     
-}
+    @Test
+    public void givenBean_checkToString() {
+        Pessoa pessoa = new Pessoa("Mary", "Jane");
+        String str = pessoa.toString();
+        assertThat(pessoa, HasToString.hasToString(str));
+    }
+
+    @Test
+    public void givenBean_checkPropertyExists() {
+        Pessoa pessoa = new Pessoa("John", "Doe");
+        assertThat(pessoa, HasProperty.hasProperty("nome"));
+    }
+
+    @Test
+    public void givenCollection_checkEmpty() {
+        List<String> emptyList = new ArrayList<>();
+        assertThat(emptyList, Matchers.empty());
+    }
+
+    @Test
+    public void givenAnInteger_checkGreater() {
+        assertThat(1, Matchers.greaterThan(0));
+    }
+
+    @Test
+    public void givenString_checkNull() {
+        String str = null;
+        assertThat(str, emptyOrNullString());
+    }
+ }
