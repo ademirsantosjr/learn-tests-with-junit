f27e4f85b5c7675a4f303ed4c83b58aa9831f922
Ademir <ademir.m.junior@gmail.com>
2021-07-09 08:48:36 -0300

    Developed a Mockito proof test

--- src/test/java/one/digitalinnovation/calculadoratest/CalculadoraTest.java
+++ src/test/java/one/digitalinnovation/calculadoratest/CalculadoraTest.java
@@ -1,16 +1,27 @@
 package one.digitalinnovation.calculadoratest;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
 import one.digitalinnovation.calculadora.Calculadora;
 
 public class CalculadoraTest {
+    
     @Test
     public void testSomar() { 
         Calculadora calc = new Calculadora();
         int soma = calc.somar("1+1+3");
         assertEquals(5, soma);
+    }
+
+    @Test
+    public void testSomarComMock() {
+        Calculadora calculadora = mock(Calculadora.class);
+        when(calculadora.somar("1+2")).thenReturn(10);
+        int resultado = calculadora.somar("1+2");
+        assertEquals(10, resultado);
     }
 }
