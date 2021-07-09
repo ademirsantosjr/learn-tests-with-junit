b5f89d1fc1ba12e8015dd22f97caffd7de8cbdbf
Ademir <ademir.m.junior@gmail.com>
2021-07-09 00:06:17 -0300

    Developed Exception tests

---
+++ src/test/java/one/digitalinnovation/exceptiontest/ExceptionTest.java
@@ -0,0 +1,41 @@
+package one.digitalinnovation.exceptiontest;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class ExceptionTest {
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void empty() {
+        new ArrayList<Object>().get(0);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void shoudTestExceptionMessage() throws IndexOutOfBoundsException {
+        List<Object> list = new ArrayList<>();
+        thrown.expect(IndexOutOfBoundsException.class);
+        thrown.expectMessage("Index 0 out of bounds for length 0");
+        list.get(0);
+    }
+
+    @Test
+    public void testExceptionMessage() {
+        try {
+            new ArrayList<Object>().get(0);
+            fail("Esperado que IndexOutOfBoundsException seja lançada.");            
+        } catch (IndexOutOfBoundsException ex) {
+            assertThat(ex.getMessage(), is("Index 0 out of bounds for length 0"));
+        }
+    }
+}
